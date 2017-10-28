fa88df4e57327a9a0d466869bd529010e2534566
MikeStyx <mike44contact@gmail.com>
2017-10-28 20:43:50 +0200

    màj pour récupérer tes codes + modifs models 3D

---
+++ TransMaze/Assets/Scripts/StationGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationGenerator : MonoBehaviour {
+
+	//Ce script sert à récupérer les infos de la gare en cours
+
+	//compte du nombre de stations: 
+	public int NbrStations;
+
+
+	// Use this for initialization
+	void Start () {
+
+		MetroHelper.CurrentStation = MetroHelper.GetRandomStation;
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		NbrStations = MetroHelper.CurrentStation.ConnectionCount;
+		Debug.Log (NbrStations);
+		
+	}
+}
